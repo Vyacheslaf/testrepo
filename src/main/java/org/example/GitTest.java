86fd147b65150bc47bcee3091b9f4e33029b2fa6
Vyacheslav Fedchenko <vyacheslav.fedchenko@gmail.com>
2022-09-19 18:17:30 +0300

    add doPost from IDEA

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -1,6 +1,10 @@
 package org.example;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class GitTest extends HttpServlet{
 
@@ -9,4 +13,8 @@ public class GitTest extends HttpServlet{
 	 */
 	private static final long serialVersionUID = 1059246847329804279L;
 
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("doPost");
+	}
 }
