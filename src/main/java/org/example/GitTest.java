000cf0a97d056d9e37c5b68521df24ecbb41b547
Vyacheslav Fedchenko <vyacheslav.fedchenko@gmail.com>
2022-09-19 18:04:11 +0300

    testrepo 2

---
+++ src/main/java/org/example/GitTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import javax.servlet.http.HttpServlet;
+
+public class GitTest extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1059246847329804279L;
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello Repo</h2>
+</body>
+</html>
